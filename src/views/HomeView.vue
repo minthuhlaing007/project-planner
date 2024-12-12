4d371f65c04c969cf24cd8599d33f1cd040c0806
minthuhlaing <minthuhlaing786@gmail.com>
2024-12-12 12:45:10 +0630

    creating fake api and fetching the data and show the result in home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
       <h1>Home Page</h1>
+      <div v-for="project in projects" :key="project.id">
+        <h3>{{ project.title }}</h3>
+        <p>{{ project.detail }}</p>
+      </div>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      projects:[]
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/projects")
+    .then(response => response.json())
+    .then(datas => this.projects = datas)
+    .catch(error => console.log(error.message))
+  }
 }
 </script>
